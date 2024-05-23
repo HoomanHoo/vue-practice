bc8516601e8cccced6caae2b5e360f18c33dd9e6
조범구 <whqjarn111@gmail.com>
2024-05-23 13:57:54 +0900

    Create Footer.vue

---
+++ src/components/layout/Footer.vue
@@ -0,0 +1,21 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <div id="footer" class="footer">이것은 푸터입니다 아마도...</div>
+</template>
+<script>
+export default {}
+</script>
+<style scoped>
+#footer {
+  max-height: 100vh;
+}
+
+.footer {
+  /* background: #f05945; */
+  /* color: white; */
+  margin: auto;
+  /* width: 30%; */
+  padding: 7px;
+  border-radius: 30px 5px 30px 5px;
+}
+</style>
