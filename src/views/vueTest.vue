4d88bbc2d80f2c2c17e8ec1bf7ec7f24a9332755
조범구 <whqjarn111@gmail.com>
2024-05-23 13:59:43 +0900

    Create vueTest.vue

---
+++ src/views/vueTest.vue
@@ -0,0 +1,28 @@
+<script setup>
+// import { ref, createApp } from 'vue'
+
+import { ref } from 'vue'
+import testVue2 from './vueTest2.vue'
+const results = ref('')
+
+async function loadCitiesList() {
+  const res = await fetch('http://localhost:3000/api/cities')
+  const jsonData = await res.json()
+  results.value = jsonData
+}
+</script>
+
+<template>
+  <div id="cityList">
+    <button @click="loadCitiesList()">도시 리스트</button>
+    <li v-for="result in results">{{ result.name }} / {{ result.location }}</li>
+    <br />
+  </div>
+  <testVue2 />
+</template>
+
+<style scoped>
+button {
+  font-weight: bold;
+}
+</style>
