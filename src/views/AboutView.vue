c0039984cc439e788fb33bf1b563854788aa2267
조범구 <whqjarn111@gmail.com>
2024-05-23 13:59:59 +0900

    Update AboutView.vue

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,17 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <!-- <div class="about"> -->
+  <h1>This is an about page</h1>
+  <!-- </div> -->
 </template>
 
-<style>
-@media (min-width: 1024px) {
+<!-- <style>
+@media (min-width: 10px) {
   .about {
-    min-height: 100vh;
-    display: flex;
+    min-height: 10vh;
+    max-height: 30vh;
+    max-width: 100vw;
+    /* display: flex; */
     align-items: center;
   }
 }
-</style>
+</style> -->
